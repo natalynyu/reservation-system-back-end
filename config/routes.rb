e66fd5f99ba9fd2faae44f6ae4b191890f79a1c7
natalynyu <natalyn.yu@gmail.com>
2019-01-31 20:20:22 -0500

    Update doctor routes
    Update routes to include update_info and get_info custom controllers

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   post '/sign-in' => 'users#signin'
   delete '/sign-out' => 'users#signout'
   patch '/change-password' => 'users#changepw'
+  patch '/update-doctor-info' => 'doctors#update_info'
+  get '/get-doctor-info' => 'doctors#get_info'
 end
