80d94c8aeea8ee4131b06c886b88b7abf95b25ca
natalynyu <natalyn.yu@gmail.com>
2019-02-10 20:20:38 -0500

    Prevent users from updating other user information
    Add permissions to prevent users from updating other users' info

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class DoctorsController < ProtectedController
-  before_action :set_doctor, only: [:show, :update, :destroy]
+  before_action :set_doctor, only: %i[show update destroy]
 
   # GET /doctors
   def index
@@ -15,7 +17,9 @@ class DoctorsController < ProtectedController
 
   # POST /doctors
   def create
-    @doctor = Doctor.new(doctor_params)
+    p = doctor_params
+    p[:user_id] = @current_user.id
+    @doctor = Doctor.new(p)
 
     if @doctor.save
       render json: @doctor, status: :created, location: @doctor
@@ -26,7 +30,9 @@ class DoctorsController < ProtectedController
 
   # PATCH/PUT /doctors/1
   def update
-    if @doctor.update(doctor_params)
+    p = doctor_params
+    p[:user_id] = @current_user.id
+    if @doctor.update(p)
       render json: @doctor
     else
       render json: @doctor.errors, status: :unprocessable_entity
@@ -39,7 +45,7 @@ class DoctorsController < ProtectedController
   end
 
   def update_info
-    @doctor = Doctor.find_by(user_id: doctor_params[:user_id])
+    @doctor = Doctor.find_by(user_id: @current_user.id)
     if @doctor
       update
     else
@@ -48,7 +54,7 @@ class DoctorsController < ProtectedController
   end
 
   def get_info
-    doctor = Doctor.find_by(user_id: params[:user_id])
+    doctor = Doctor.find_by(user_id: @current_user.id)
     if doctor.nil?
       render nothing: true, status: 204
     else
@@ -60,12 +66,11 @@ class DoctorsController < ProtectedController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_doctor
-    @doctor = Doctor.find(params[:id])
+    @doctor = @current_user.doctors.find(params[:id])
   end
 
   # Only allow a trusted parameter "white list" through.
   def doctor_params
-    params.require(:doctor).permit(:full_name, :department, :user_id)
+    params.require(:doctor).permit(:full_name, :department)
   end
-
 end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,14 +1,14 @@
+# frozen_string_literal: true
+
 class ReservationsController < ProtectedController
-  before_action :set_reservation, only: [:show, :update, :destroy]
+  before_action :set_doctor
+  before_action :set_reservation, only: %i[show update destroy]
 
   # GET /reservations
   def index
     # find doctor record from @current_user
     # find reservation records where doctor_id is equal to current doctor id
-    doctor = Doctor.find_by(user_id: @current_user.id)
-
-    @reservations = Reservation.where(doctor_id: doctor.id)
-
+    @reservations = Reservation.where(doctor_id: @doctor.id)
     render json: @reservations
   end
 
@@ -19,20 +19,27 @@ class ReservationsController < ProtectedController
 
   # POST /reservations
   def create
+    if @doctor.nil?
+      render json: { reservation: 'cannot be created before doctor info is entered' }, status: :unprocessable_entity
+      return
+    end
     rp = reservation_params
     machine = rp[:machine]
     start_time = rp[:start_time]
     end_time = rp[:end_time]
     # NOTE: (some expression) OR (some other expression)
     # like (machine = ? and start_time = ?) or (machine = ? and start_time < ?)
-    reservations = Reservation.where('machine = :machine and
-      :end_time > start_time and :start_time < end_time',
-      {machine: machine, start_time: start_time, end_time: end_time})
+    reservations = Reservation.where(
+      'machine = :machine and :end_time > start_time and
+        :start_time < end_time',
+      machine: machine, start_time: start_time, end_time: end_time
+    )
     if reservations.any?
       render json: { reservation: 'already exists' }, status: :unprocessable_entity
       return
     end
-    @reservation = Reservation.new(reservation_params)
+    rp[:doctor_id] = @doctor.id
+    @reservation = Reservation.new(rp)
 
     if @reservation.save
       render json: @reservation, status: :created, location: @reservation
@@ -43,6 +50,7 @@ class ReservationsController < ProtectedController
 
   # PATCH/PUT /reservations/1
   def update
+    rp = reservation_params
     if @reservation.update(reservation_params)
       render json: @reservation
     else
@@ -56,15 +64,22 @@ class ReservationsController < ProtectedController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_reservation
-      @reservation = Reservation.find(params[:id])
-      # @reservation = Reservation.find_by(id: params[:id], doctor_id: @current_user.id)
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    # {reservation: {start_time: , end}}
-    def reservation_params
-      params.require(:reservation).permit(:start_time, :end_time, :machine, :doctor_id)
-    end
+  def set_doctor
+    @doctor = Doctor.find_by(user_id: @current_user.id)
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_reservation
+    return if @doctor.nil?
+
+    @reservation = @doctor.reservations.find(params[:id])
+    # @reservation = @current_user.doctorsReservation.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  # {reservation: {start_time: , end}}
+  def reservation_params
+    params.require(:reservation).permit(:start_time, :end_time, :machine)
+  end
 end
